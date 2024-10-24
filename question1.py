e0ea2b393dcb0375c6de08503b6e56d6bf4b5113
ayomi-20 <peaceayomirwoth@gmail.com>
2024-10-24 11:10:53 +0300

    python test

--- question1.py
+++ question1.py
@@ -1,5 +1,14 @@
 # Question 1(i)
 # Write a Python program to find the distance between two coordinate points (x1, y1) and (x2, y2).
+import math
+squareroot = math.sqrt
+x1 = int(input("Enter horizontal piont 1: "))
+x2 = int(input("Enter horizontal point 2: "))
+y1 = int(input("Enter vertical piont 1: "))
+y2 = int(input("Enter vertical piont 2: "))
+distance = squareroot((x2 - x1)**2) + ((y2 - y1)**2)
+print(f"The distance between the two coordinate points(x and y) is {distance}")
+
 
 
 
@@ -9,6 +18,15 @@
 
 # Question 1(ii)
 # Write a Python program to find maximum between three numbers.
+number1 = int(input("Enter the first number: "))
+number2 = int(input("Enter the second number: "))
+number3 = int(input("Enter the third number: "))
+if number1 > number2 and number1 > number3:
+    print(f"The maximum number is {number1}")
+elif number2 > number1 and number2 > number3:
+    print(f"The maximum number is {number2}")
+else:
+    print(f"The maximum number is {number3}")
 
 
 
--- question2.py
+++ question2.py
@@ -7,3 +7,18 @@
 # Percentage >= 60%: Grade D
 # Percentage >= 40%: Grade E
 # Percentage < 40%:  Grade F
+def student_grading_system():
+    student_mark = int(input("Enter the mark you scored: "))
+    if student_mark >= 90 and student_mark <= 100:
+        print("The student grade you obtained is A")
+    elif student_mark >= 80 and student_mark <= 89:
+        print("The student grade you obtained is B")
+    elif student_mark >= 70 and student_mark <= 79:
+        print("The student grade you obtained is C")
+    elif student_mark >= 60 and student_mark <= 69:
+        print("The student grade you obtained is D")
+    elif student_mark >= 40 and student_mark <= 59:
+        print("The student grade you obtained is E")
+    else:
+        print("The student grade you obtained is F")
+student_grading_system()
