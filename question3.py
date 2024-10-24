671705b110e94b03e00e483feb766f8e0015e57d
ayomi-20 <peaceayomirwoth@gmail.com>
2024-10-24 11:27:38 +0300

    python test

--- question3.py
+++ question3.py
@@ -1,16 +1,12 @@
 # Question 3(i)
 #  Write a Python program that prompts a user to enter numbers. The process will repeat until
 #  the user enters 0. Finally, the program prints sum of the numbers entered by the user.
-def number():
-    number != 0
-    return number
-
-while number == 0:
-    print(f"The sum of numbers entered by the user is {sum}")
-    initial_number = 0
 number = int(input("Enter desired number: "))
-sum =+ initial_number
-number()
+initial_number = 0
+while number > 0:
+    sum = number + initial_number
+    if number == 0:
+        print(f"The sum of numbers entered by the user is {sum}")
 
 
 
